c3b1126cf441a9f8b713ed5b1ea181c16a4aacb8
Aisha Awudu <awuduaisha@gmail.com>
2023-03-09 02:10:29 -0800

    fixed .format error in 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for foo1 in range(0, 10):
     for foo2 in range(0, 10):
         if foo1 == 8 & foo2 == 9:
-            print(f'{foo1}{foo2}')
+            print("{}{}".format(foo1, foo2))
         elif foo1 < foo2:
-            print(f'{foo1}{foo2}', end=',')
+            print("{}{}".format(foo1, foo2), end=', ')
