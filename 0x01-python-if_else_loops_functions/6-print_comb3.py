a5997b4d92513448b9bfd463d4f5e0dd8f5a3761
Aisha Awudu <awuduaisha@gmail.com>
2023-03-08 16:09:04 -0800

    added 6-print_comb3 file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for foo1 in range(0, 10):
+    for foo2 in range(0, 10):
+        if foo1 == 8 & foo2 == 9:
+            print(f'{foo1}{foo2}')
+        elif foo1 < foo2:
+            print(f'{foo1}{foo2}', end=',')
