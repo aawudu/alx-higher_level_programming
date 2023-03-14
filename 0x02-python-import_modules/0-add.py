1a8d2bf2e45660d1bd0514ea172f7c7c4a51087c
Aisha Awudu <awuduaisha@gmail.com>
2023-03-14 02:43:46 -0700

    added 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0_add.py
+# Aisha Awudu <awuduaisha@gmail.com>
+if _name_ == "_main_":
+    """results is sum of a and b"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
